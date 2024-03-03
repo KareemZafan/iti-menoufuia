d10cb90c7f77c5ed1f0a241ee41c96cc7e35007d
KareemZafan <kareemm.zafan@gmail.com>
2024-03-03 13:57:13 +0200

    Math-feature-123 square root

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
-#include<stdio.h> 
+#include <stdio.h> 
+#include <math.h>
 
 int main(void)
 {
@@ -6,6 +7,11 @@ int main(void)
   printf("Mai added her changes");
   printf("Mai added her changes");
   
+  /***
+  These are the changes for newBranch 
+  ***/
+  
+  printf("d",sqrt(81));  
   
 return 0;
 }
